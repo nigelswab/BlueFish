02bc584e29ba00a578dd0fbb6bba80e7d624799c
Nigel <nigelswab@uvic.ca>
2021-03-06 21:10:38 -0800

    WIP
    Trying read

---
+++ Sandbox/Pi/Read_and_Parse.py
@@ -0,0 +1,8 @@
+import pyserial, numpy
+
+arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
+
+while True:
+	data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
+	if data:
+		print(data)
