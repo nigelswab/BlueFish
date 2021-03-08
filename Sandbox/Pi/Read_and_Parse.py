50d123c2d642bcf068e28e77efef21e5d4f7758c
nigelswab <nigelswab@uvic.ca>
2021-03-07 21:14:44 -0800

    < can read serial and break the code with CNTRL+C>

--- Sandbox/Pi/Read_and_Parse.py
+++ Sandbox/Pi/Read_and_Parse.py
@@ -1,8 +1,17 @@
-import serial, numpy
+import serial
+import time
+import csv
 
 arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
+arduino.flushInput()
 
 while True:
-	data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
-	if data:
-		print(data)
+    try:
+        arduino_bytes = arduino.readline()
+        decoded_bytes = (arduino_bytes[0:len(arduino_bytes)-2].decode("utf-8"))
+        print(decoded_bytes)
+        
+    except:
+        print("Error: Keyboard Interrupt")
+        break   
+        
