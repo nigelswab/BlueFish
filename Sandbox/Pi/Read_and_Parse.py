3ab4885095363adf618107aef92dc1ef01ea5efe
nigelswab <nigelswab@uvic.ca>
2021-03-12 01:12:25 -0800

    <Updated pi code>

--- Sandbox/Pi/Read_and_Parse.py
+++ Sandbox/Pi/Read_and_Parse.py
@@ -2,16 +2,37 @@ import serial
 import time
 import csv
 
+# set usb port name, naud rate, and timeout so program doesn't get stuck
 arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
-arduino.flushInput()
 
+# get rid of garbage/incomplete data
+arduino.flush()
+
+# create a firle name and open to be appended 
+fileName="serial-test.csv" 
+file = open(fileName, "a")
+print(fileName + " created")
+
+# Infinite loop
 while True:
     try:
-        arduino_bytes = arduino.readline()
-        decoded_bytes = (arduino_bytes[0:len(arduino_bytes)-2].decode("utf-8"))
-        print(decoded_bytes)
+        # Read until new line char and convert byte data into string,
+        # removing trailing chars like new line '\n'
+        line = arduino.readline().decode('utf-8').rstrip()
+        print(line)
         
+        
+        #append the data to the file
+        file = open(fileName, "a")
+        #write data with a new line
+        file.write(line + "\\n") 
+        
+        
+
     except:
+        #close out the file
+        file.close()
+        
         print("Error: Keyboard Interrupt")
         break   
         
