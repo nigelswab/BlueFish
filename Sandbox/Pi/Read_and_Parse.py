f605a8b8ad4f6b18bba5aa92102b64292e1042d3
nigelswab <nigelswab@uvic.ca>
2021-03-14 17:21:20 -0700

    created Logger class in csv_logger.py and played with servos/control loop

--- Sandbox/Pi/Read_and_Parse.py
+++ Sandbox/Pi/Read_and_Parse.py
@@ -1,6 +1,18 @@
+import os
 import serial
 import time
 import csv
+from datetime import datetime
+
+import csv_logger
+
+fileName = "Testing Logging"
+test_plan = '' # link to word test_plan on onedrive
+test_notes = '' # anything of note for this test
+firmware = '' #to keep track of updates -> use firmware id in git commits so revision can be returned to if needed
+#tuning_parameters = '' #PID variable levels (ki kp etc)
+
+log= csv_logger.Logger(fileName, test_plan, test_notes, firmware)
 
 # set usb port name, naud rate, and timeout so program doesn't get stuck
 arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
@@ -8,31 +20,13 @@ arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
 # get rid of garbage/incomplete data
 arduino.flush()
 
-# create a firle name and open to be appended 
-fileName="serial-test.csv" 
-file = open(fileName, "a")
-print(fileName + " created")
-
 # Infinite loop
 while True:
-    try:
-        # Read until new line char and convert byte data into string,
-        # removing trailing chars like new line '\n'
+    
+        # Read until new line char and convert byte data into string, and log into csv
         line = arduino.readline().decode('utf-8').rstrip()
+        log.log_row(line)
         print(line)
-        
-        
-        #append the data to the file
-        file = open(fileName, "a")
-        #write data with a new line
-        file.write(line + "\\n") 
-        
-        
-
-    except:
-        #close out the file
-        file.close()
-        
-        print("Error: Keyboard Interrupt")
-        break   
+    
+          
         
---
+++ Sandbox/Pi/csv_logger.py
@@ -0,0 +1,58 @@
+import os
+import serial
+import time
+import csv
+from datetime import datetime
+
+
+
+class Logger:
+    def __init__(self, fileName: str,
+                 test_plan: str,
+                 test_notes: str,
+                 firmware: str):
+        
+        self.fileName = self.remove_bad_chars(fileName)
+        self.test_plan = test_plan
+        self.test_notes = test_notes
+        self.firmware = firmware
+        
+        # set folder and file path
+        self.folderPath ='/home/pi/BlueFish/data/' + datetime.today().strftime("%Y-%m-%d") 
+        self.filePath = self.folderPath + '/' + datetime.today().strftime('%Y-%m-%d - %H:%M:%S') + ' - ' + self.fileName + '.csv'
+
+        # create folder (and file) as needed
+        if not os.path.exists(self.folderPath):
+            os.mkdir(self.folderPath)
+        self.file = open(self.filePath, "a")
+        print(self.filePath + " created")
+        
+        # Insert metadata
+        standard_meta = {'start_timestamp': datetime.today().strftime('%Y-%m-%d - %H:%M:%S'),
+                         'test_plan': self.test_plan,
+                         'firmware': self.firmware,
+                         'test_notes': self.test_notes or ''
+                         }
+        
+        # insert meta into the top of the csv
+        for meta in standard_meta:
+            #append the data to the file
+            self.file = open(self.filePath, "a")
+            #write data with a new line
+            self.file.write(meta + "," + standard_meta[meta] + '\n')
+        self.file.close()
+
+
+    def log_row(self, data: str):
+        #append the data to the file
+        self.file = open(self.filePath, "a")
+        #write data with a new line
+        self.file.write(data + "\n") 
+                
+    
+    def remove_bad_chars(self, string: str) -> str:
+        for char in string:
+            if char in r'\/:*?"<>|':
+                string = string.replace(char, '')
+        return string
+
